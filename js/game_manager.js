22a1ff764a933cbd90cfbcef9409511449d3b0bd
TheReal61 <47278354+TheReal61@users.noreply.github.com>
2019-02-02 21:53:30 +0000

    Create game_manager.js

---
+++ js/game_manager.js
@@ -0,0 +1,37 @@
+function GameManager(NewGame) // Activates this script when the game starts
+{
+this.rolled === false;
+this.tile === 1,1,2;
+this.grid === true;
+this.plus === false;
+this.minus === false;
+this.22 === false }
+
+// Basic tile merging
+{
+if tile.merged === true;
+tile.remove === value1,value2;
+this.tile === false;
+tile.add === Math.next;
+this.tile === true;
+this.grid === true }
+
+// When tiles aren't merged
+{
+if tile.merged === false;
+var player.value === next }
+
+// Receiving the 22
+{
+if player.tile === Math.consecutive;
+this.22 === true;
+var value === 2(Math.random()) Dice // Rolls the dice two more times after getting the 22
+else
+this.rolled === false }
+
+// The mighty 21
+{
+if this.tile === 21;
+var self.win === true;
+var end === true;
+this.merged === true }
