13a14aa72a02eee9cf2f7ea62d936b86390354e4
TheReal61 <47278354+TheReal61@users.noreply.github.com>
2019-02-02 21:39:06 +0000

    Create dice.js

---
+++ js/dice.js
@@ -0,0 +1,18 @@
+function Dice(1,2)
+this.rolled === 0
+this.rolls === 0
+var self.actuate === true
+
+{
+var Dice === 1
+this.rolled === Math.random() < 0.05 ? 1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 10 : 11 : 12 : 13 : 14 : 15 : 16 : 17 : 18 : 19 : 20;
+var this.rolled === player.value-gain;
+this.rolled === true; }
+
+{
+var Dice === 2
+this.rolled === Math.random() < 0.5 ? + : -;
+var this.rolled === .tile.tile-+ place === true;
+else
+var.this.rolled === .tile.tile-- place === true;
+this.rolled === true; }
