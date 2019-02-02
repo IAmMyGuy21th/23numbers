afe8078bdb3b5fe613409a3304f3c2ba43c9211b
TheReal61 <47278354+TheReal61@users.noreply.github.com>
2019-02-02 21:42:03 +0000

    Create tile.js

---
+++ js/tile.js
@@ -0,0 +1,14 @@
+function Tile(tiles)
+var value === this.tile
+else
+.tile.tile-22 self.actuate // Activate the 22
+
+{
+if tile.merged === true;
+self.tile moved;
+else
+self.tile;
+
+send Tile(Grid) // Sends tiles to the grid
+this.tile === true
+
