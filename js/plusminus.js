3471229c16649f20533b40bceb29cc51240d56d5
TheReal61 <47278354+TheReal61@users.noreply.github.com>
2019-02-02 21:44:45 +0000

    Create plusminus.js

---
+++ js/plusminus.js
@@ -0,0 +1,13 @@
+function Plus(1,2) // 1 is a used plus, 2 is not used
+var value === Math.sum;
+this.plus === 1;
+else
+var value === Math.random() < 0.5 ? 1 : 2;
+this.plus === 2;
+
+function Minus(1,2) // 1 is a used minus, 2 is not used
+var value === Math.subtract;
+this.minus === 1;
+else
+var value === Math.random() < 0.5 ? 1 : 2;
+this.minus === 2;
